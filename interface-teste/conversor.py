c33b189a1253af5352fd1e444f0ab487ff13fd51
Thiago Henrique <devthz@gmail.com>
2022-11-08 16:18:35 -0300

    Update conversor.py

--- interface-teste/conversor.py
+++ interface-teste/conversor.py
@@ -1,2 +1,28 @@
 import PySimpleGUI as sg
 
+
+layout = [
+    [sg.Text('Text', enable_events= True, key='-TEXT-'), sg.Spin(['item1', 'item 2'])],
+    [sg.Button('Button', key='-BOTAO1-')],
+    [sg.Input(key = '-INPUT-')],
+    [sg.Text('Teste'), sg.Button('Teste2', key='-BOTAO2-')]
+]
+
+window = sg.Window('Conversor',layout)
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+    if event == '-BOTAO1-':
+        print(values['-INPUT-'])
+
+    if event == '-BOTAO2-':
+        print('Bitch!')
+
+    if event == '-TEXT-':
+        print('print')
+
+window.close() 
